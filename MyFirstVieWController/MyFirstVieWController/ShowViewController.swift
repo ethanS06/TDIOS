7a1db0f023547cf4cfebbcfd644b3eb153c5471b
ethanS06 <74767129+ethanS06@users.noreply.github.com>
2021-12-01 10:10:05 +0100

    Add files via upload

---
+++ MyFirstVieWController/MyFirstVieWController/ShowViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ShowViewController.swift
+//  MyFirstVieWController
+//
+//  Created by Ethan Sebag on 01/12/2021.
+//
+
+import UIKit
+
+class ShowViewController: UIViewController {
+    
+    
+
+    @IBOutlet weak var picture: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
